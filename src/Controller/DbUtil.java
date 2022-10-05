a00cf2986eeaf40f7fe09168603d9ff4c3c1e50d
Paulo-Guerrero <88761261+Paulo-Guerrero@users.noreply.github.com>
2022-10-05 12:37:35 -0400

    Updated db endpoint

--- src/Controller/DbUtil.java
+++ src/Controller/DbUtil.java
@@ -5,8 +5,8 @@ import java.sql.*;
 public class DbUtil {
     private String username = "admin";
     private String password = "password123";
-    private String databaseName = "";
-    private String connectionAddress = "";
+    private String endpoint = "workouttracker.cvi29upq4yqe.us-east-1.rds.amazonaws.com/workoutTracker";
+    private String connectionAddress = "jdbc:mysql://" + endpoint;
 
     public ResultSet executeQuery(String query) throws SQLException {
         Connection connection = DriverManager.getConnection(connectionAddress, username, password);
