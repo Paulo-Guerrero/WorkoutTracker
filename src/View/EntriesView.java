5b99576c7eaf2dafb788603d3d5c6614ef4ff984
Paulo-Guerrero <88761261+Paulo-Guerrero@users.noreply.github.com>
2022-10-07 19:33:32 -0400

    Save button to entries

--- src/View/EntriesView.java
+++ src/View/EntriesView.java
@@ -1,5 +1,6 @@
 package View;
 
+import Controller.EntryController;
 import Controller.LiftController;
 
 import java.awt.event.ActionEvent;
@@ -11,6 +12,7 @@ public class EntriesView extends PageView implements ActionListener {
     private JButton addRecord = new JButton("Add Record");
     private JLabel dateHeader = new JLabel(LocalDate.now().toString());
     private JButton homeButton = new JButton("Home");
+    private JButton saveButton = new JButton("Save Entry");
     private LiftController liftController = new LiftController();
 
     public EntriesView(JFrame frame, JPanel panel) {
@@ -21,9 +23,14 @@ public class EntriesView extends PageView implements ActionListener {
         this.panel.removeAll();
         this.panel.add(homeButton);
         this.panel.add(dateHeader);
+        this.panel.add(new JLabel("Movement"));
+        this.panel.add(new JLabel("Reps"));
+        this.panel.add(new JLabel("Value"));
         this.panel.add(addRecord);
+        this.panel.add(saveButton);
         addRecord.addActionListener(this);
         homeButton.addActionListener(this);
+        saveButton.addActionListener(this);
         this.panel.revalidate();
     }
 
@@ -32,11 +39,15 @@ public class EntriesView extends PageView implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == addRecord){
             this.panel.add(new JComboBox(liftController.getLifts()));
+            this.panel.add(new JTextField(2));
             this.panel.add(new JTextField(10));
             this.panel.revalidate();
         } else if (e.getSource() == homeButton){
             HomePageView homePageView = new HomePageView(this.frame, this.panel);
             homePageView.renderView();
+        }else if (e.getSource() == saveButton){
+            EntryController entryController = new EntryController();
+            entryController.saveEntry();
         }
     }
 }
